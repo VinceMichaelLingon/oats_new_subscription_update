d7e43bb0c52fa8b8999653a5fb3a3e5dd49bdb6a
vincemichaellingon <vincemichaellingon@gmail.com>
2022-05-17 22:08:01 +0800

    added new

--- app/src/main/java/com/example/oatsv5/MainActivity.java
+++ app/src/main/java/com/example/oatsv5/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 //            finish();
 //           Intent intent = new Intent(this, MainActivity.class);
 
-            Intent home = new Intent(getApplicationContext(),MainAuth.class);
+            Intent home = new Intent(getApplicationContext(),DashboardActivity.class);
             startActivity(home);
             finish();
 
